2905ed3d5be1f27d7ca14896278a027d1b99daaa
hurghff <jmsabio@evc.pshs.edu.ph>
2026-06-09 16:40:19 +0800

    initial commit

---
+++ vuetify-project-test/src/pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <!-- <HelloWorld /> -->
+  <Test />
+</template>
+
+<script lang="ts" setup>
+  import HelloWorld from '@/components/HelloWorld.vue'
+  import Test from '@/components/Test.vue'
+</script>
